37151f8b2debdfdc8dc781f846e70e59d24b53c8
vaske <shomi199870@gmail.com>
2020-10-24 11:40:53 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace St0rmPasscode
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+                Application.Run(new VASKE());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ VASKE.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace St0rmPasscode
+{
+    partial class VASKE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.lblBuild = new System.Windows.Forms.Label();
+            this.lbliOS = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCheckme = new System.Windows.Forms.Label();
+            this.linkLabelAds = new System.Windows.Forms.LinkLabel();
+            this.serial = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Backup Activation";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 43);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Activate Device";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Location = new System.Drawing.Point(246, 190);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(47, 13);
+            this.lblWarning.TabIndex = 17;
+            this.lblWarning.Text = "Warning";
+            this.lblWarning.Visible = false;
+            // 
+            // lblBuild
+            // 
+            this.lblBuild.AutoSize = true;
+            this.lblBuild.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuild.Location = new System.Drawing.Point(227, 132);
+            this.lblBuild.Name = "lblBuild";
+            this.lblBuild.Size = new System.Drawing.Size(48, 16);
+            this.lblBuild.TabIndex = 9;
+            this.lblBuild.Text = "12.4.8";
+            this.lblBuild.Visible = false;
+            // 
+            // lbliOS
+            // 
+            this.lbliOS.AutoSize = true;
+            this.lbliOS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbliOS.Location = new System.Drawing.Point(145, 132);
+            this.lbliOS.Name = "lbliOS";
+            this.lbliOS.Size = new System.Drawing.Size(48, 16);
+            this.lbliOS.TabIndex = 10;
+            this.lbliOS.Text = "12.4.8";
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductName.Location = new System.Drawing.Point(145, 59);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(81, 16);
+            this.lblProductName.TabIndex = 11;
+            this.lblProductName.Text = "iPhone OS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "ProductType:";
+            // 
+            // lblModel
+            // 
+            this.lblModel.AutoSize = true;
+            this.lblModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModel.Location = new System.Drawing.Point(145, 96);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(64, 16);
+            this.lblModel.TabIndex = 13;
+            this.lblModel.Text = "iPhone6";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType.Location = new System.Drawing.Point(145, 96);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(76, 16);
+            this.lblType.TabIndex = 14;
+            this.lblType.Text = "iPhone7,2";
+            this.lblType.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "DeviceName:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "ProductVersion:";
+            // 
+            // lblCheckme
+            // 
+            this.lblCheckme.AutoSize = true;
+            this.lblCheckme.Location = new System.Drawing.Point(225, 26);
+            this.lblCheckme.Name = "lblCheckme";
+            this.lblCheckme.Size = new System.Drawing.Size(79, 13);
+            this.lblCheckme.TabIndex = 8;
+            this.lblCheckme.Text = "Checking con..";
+            // 
+            // linkLabelAds
+            // 
+            this.linkLabelAds.DisabledLinkColor = System.Drawing.Color.Black;
+            this.linkLabelAds.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.linkLabelAds.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
+            this.linkLabelAds.LinkColor = System.Drawing.Color.Red;
+            this.linkLabelAds.Location = new System.Drawing.Point(3, 3);
+            this.linkLabelAds.Name = "linkLabelAds";
+            this.linkLabelAds.Size = new System.Drawing.Size(539, 23);
+            this.linkLabelAds.TabIndex = 18;
+            this.linkLabelAds.Text = "by vaske";
+            this.linkLabelAds.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkLabelAds.VisitedLinkColor = System.Drawing.Color.Red;
+            // 
+            // serial
+            // 
+            this.serial.AutoSize = true;
+            this.serial.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serial.Location = new System.Drawing.Point(145, 167);
+            this.serial.Name = "serial";
+            this.serial.Size = new System.Drawing.Size(29, 16);
+            this.serial.TabIndex = 13;
+            this.serial.Text = "SN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "SerialNumber:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 271);
+            this.Controls.Add(this.linkLabelAds);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.lblBuild);
+            this.Controls.Add(this.lbliOS);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.serial);
+            this.Controls.Add(this.lblModel);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCheckme);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(560, 310);
+            this.MinimumSize = new System.Drawing.Size(560, 310);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblWarning;
+        private System.Windows.Forms.Label lblBuild;
+        private System.Windows.Forms.Label lbliOS;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCheckme;
+        private System.Windows.Forms.LinkLabel linkLabelAds;
+        private System.Windows.Forms.Label serial;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
